511f66cb0c84b85eac69523bbb2e7ea4b9ee08e6
saurabhkumar1432 <saurabhkumar1432001@gmail.com>
2022-09-30 18:34:42 +0530

    default spring initializr with dependencies

---
+++ src/main/java/com/jobportal/jobportalbackend/JobPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jobportal.jobportalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobportal/jobportalbackend/JobPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobportal.jobportalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
